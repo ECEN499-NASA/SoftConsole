9a9d13b612b5db28a95423a42672bd60600f8a13
Michael Ashford <michaeljashford@icloud.com>
2020-03-20 13:42:30 -0600

    Update LCD test

--- NASA_RISC-V_TMR_TEST_PROG/lcd_test_files/lcd_test.c
+++ NASA_RISC-V_TMR_TEST_PROG/lcd_test_files/lcd_test.c
@@ -1,9 +1,11 @@
+#include <time.h>
 #include "lcd_test.h"
 #include "spi_test_prog.h"
 
 void lcd_command(int command)
 {
-	//Take control of SPI
+	UART_polled_tx_string(&g_uart, (const uint8_t *)"\n\rSelect LCD Screen\n\r");
+	spi_test_change_selected_device();
 	//The RS pin on the LCD needs to be set to 0
 	for(int serialcounter = 1; serialcounter <= 8; serialcounter++)
 	{
@@ -20,39 +22,40 @@ void lcd_command(int command)
 		//SerialClock pin to 1
 		//SerialClock pin to 0
 	}
-	//Release control of SPI
 }
 
 void lcd_write(int message)
 {
-	//Take control of SPI
-		//The RS pin on the LCD needs to be set to 1
-		for(int serialcounter = 1; serialcounter <= 8; serialcounter++)
+	UART_polled_tx_string(&g_uart, (const uint8_t *)"\n\rSelect LCD Screen\n\r");
+	spi_test_change_selected_device();
+	//The RS pin on the LCD needs to be set to 1
+	for(int serialcounter = 1; serialcounter <= 8; serialcounter++)
+	{
+		if((message&0x80)==0x80)
 		{
-			if((message&0x80)==0x80)
-			{
-				//SerialInput pin set to 1
-			}
-			else
-			{
-				//SerialInput pin set to 0
-			}
-			message = (message<<1);
-			//SerialClock pin to 0
-			//SerialClock pin to 1
-			//SerialClock pin to 0
+			//SerialInput pin set to 1
 		}
-		//Release control of SPI
+		else
+		{
+			//SerialInput pin set to 0
+		}
+		message = (message<<1);
+		//SerialClock pin to 0
+		//SerialClock pin to 1
+		//SerialClock pin to 0
+	}
 }
 
 void lcd_init(void)
 {
+	UART_polled_tx_string(&g_uart, (const uint8_t *)"\n\rSelect LCD Screen\n\r");
+	spi_test_change_selected_device();
 	//LCD Rst to 0
-	//delay(2);
+	nanosleep((const struct timespec[]){{0, 2000000L}}, NULL);
 	//LCD Rst to 1
-	//delay(20);
+	nanosleep((const struct timespec[]){{0, 20000000L}}, NULL);
 	lcd_command(0x30);
-	//delay(2);
+	nanosleep((const struct timespec[]){{0, 2000000L}}, NULL);
 	lcd_command(0x30);
 	lcd_command(0x30);
 	lcd_command(0x39);
