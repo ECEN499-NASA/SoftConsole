fff0c616b472d21f54db0a231d2a7928d135c0bb
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2020-07-13 14:39:37 -0700

    sh

--- git commit/commit_kyle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd D:/Project-499/SoftConsole
-git add --all
-timestamp() {
-  date +"at  %H:%M on %m/%d/%Y"
-}
-git commit -am "auto-commit : Update by Kyle $(timestamp)"
-git pull
-git push origin master
