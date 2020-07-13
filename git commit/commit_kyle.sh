7c0de3ae4eb63a980fb45d30a754fedba5e2a296
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2020-07-13 14:20:16 -0700

    auto-commit : Update by Kyle at  14:20 on 07/13/2020

--- git commit/commit_kyle.sh
+++ git commit/commit_kyle.sh
@@ -2,7 +2,7 @@
 cd D:/Project-499/SoftConsole
 git add --all
 timestamp() {
-  date +"at  %H:%M on %d/%m/%Y"
+  date +"at  %H:%M on %m/%d/%Y"
 }
 git commit -am "auto-commit : Update by Kyle $(timestamp)"
 git pull
